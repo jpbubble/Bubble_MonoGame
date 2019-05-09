7f0f9ad974c5e3a99de182fb1cd9c58a527304ee
Jeroen P. Broks <facebook@tbbs.nl>
2019-05-09 19:40:12 +0200

    Error skeleton

---
+++ src/Flow/Error.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bubble {
+    class Error:HardFlowClass {
+
+        public override void Draw() {
+            throw new NotImplementedException();
+        }
+
+        public override void Update() {
+            throw new NotImplementedException();
+        }
+    }
+}
