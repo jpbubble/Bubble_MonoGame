e75a78f49ba5c6053f35f6879f7f20f8fdf7db36
Jeroen P. Broks <facebook@tbbs.nl>
2019-05-09 22:45:06 +0200

    Death

--- src/Flow/Error.cs
+++ src/Flow/Error.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
+using TrickyUnits;
 
 namespace Bubble {
     class Error:HardFlowClass {
 
+        static TQMGImage Death = null;
+
+        static void GoError(string ct, string message, string trace) {
+            Death = TQMG.GetImage(QuickStream.OpenEmbedded("Death.png"));
+        }
+
         public override void Draw(GameTime gameTime) {
-            throw new NotImplementedException();
+            TQMG.Color(0, 18, 25);
+            TQMG.DrawRectangle(0, 0, TQMG.ScrWidth, TQMG.ScrHeight);
+            Death.Draw(0, 0);
         }
 
         public override void Update(GameTime gameTime) {
