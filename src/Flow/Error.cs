7d7c09f2eb20d81b36879bcf29b6d56a27db06a6
Jeroen P. Broks <facebook@tbbs.nl>
2019-05-09 19:42:20 +0200

    GameTime

--- src/Flow/Error.cs
+++ src/Flow/Error.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 
 namespace Bubble {
     class Error:HardFlowClass {
 
-        public override void Draw() {
+        public override void Draw(GameTime gameTime) {
             throw new NotImplementedException();
         }
 
-        public override void Update() {
+        public override void Update(GameTime gameTime) {
             throw new NotImplementedException();
         }
     }
