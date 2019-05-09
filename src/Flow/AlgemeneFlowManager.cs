1400fb00b74ee250c2b68df93270fafee79432c2
Jeroen P. Broks <facebook@tbbs.nl>
2019-05-09 19:32:49 +0200

    Flow skeleton code

---
+++ src/Flow/AlgemeneFlowManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Bubble {
+
+    abstract class HardFlowClass {
+        abstract public void Draw();
+        abstract public void Update();
+    }
+
+
+
+    static class FlowManager {
+
+        static Dictionary<string, HardFlowClass> HardFlow = new Dictionary<string, HardFlowClass>();
+
+    }
+}
