5fe4875dbcd83f128662d03e4116f02df81ccf5e
Jeroen P. Broks <facebook@tbbs.nl>
2019-05-19 19:37:43 +0200

    Input

---
+++ src/dep/Bubble/Class/Bubble_Input.cs
@@ -0,0 +1,62 @@
+// Lic:
+// Bubble for MonoGame
+// Input API
+// 
+// 
+// 
+// (c) Jeroen P. Broks, 
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// Please note that some references to data like pictures or audio, do not automatically
+// fall under this licenses. Mostly this is noted in the respective files.
+// 
+// Version: 19.05.19
+// EndLic
+
+ï»¿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TrickyUnits;
+using Microsoft.Xna.Framework.Input;
+
+namespace Bubble {
+    class Bubble_Input {
+        static public void Init(string state) => new Bubble_Input(state);
+        private Bubble_Input(string state) {
+            var script = QuickStream.StringFromEmbed("Input.lua");
+            SBubble.State(state).state["Bubble_Input"] = this;
+            SBubble.DoNIL(state,"#macro MouseLeft 1\n#macro MouseRight 2\n#macro MouseCenter 3","Mouse Button macros");
+            SBubble.State(state).DoString(script, "Input API header");
+        }
+
+        public int X => FlowManager.MS.X;
+        public int Y => FlowManager.MS.Y;
+        public bool Held(byte b) {
+            switch (b) {
+                case 1:
+                    return FlowManager.MS.LeftButton == ButtonState.Pressed;
+                case 2:
+                    return FlowManager.MS.RightButton == ButtonState.Pressed;
+                case 3:
+                    return FlowManager.MS.MiddleButton == ButtonState.Pressed;
+                default:
+                    SBubble.MyError("HEY!", "I don't know what you mean by mouse button #{b}", "There's only 1,2 and 3!");
+                    return false;
+
+            }
+        }
+    }
+}
+
---
+++ src/dep/Bubble/NIL/Input.lua
@@ -0,0 +1,56 @@
+--[[
+src/dep/Bubble/NIL/Input.lua
+Copyright (C)  Jeroen P. Broks
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+Version 19.05.19
+]]
+
+Mouse = {}
+Keyboard = {}
+-- TODO: Joystick/Joypad
+
+local MetaMouse = {
+	__index = function(t,k)
+		local key=k:upper()
+		if key=="X" then
+			return Bubble_Input.X
+		elseif key=="Y" then
+			return Bubble_Input.Y
+		elseif key=="HELDLEFT" then
+			return Bubble_Input:Held(1)
+		elseif key=="HELDRIGHT" then
+			return Bubble_Input:Held(2)
+		elseif key=="HELDCENTER" then
+			return Bubble_Input:Held(3)
+		elseif key=="MOUSEINSIDE" then
+			return Bubble_Input.X>=0 and Bubble_Input.Y>0 and Bubble_Input.X<Screen.Width and Bubble_Input.Y<Screen.Height
+		else
+			BubbleCrash("Mouse."..k.." does not exist!")
+		end
+	end
+
+}
+setmetatable(Mouse,MetaMouse)
+
+
+local MetaKeyboard = {
+
+}
+setmetatable(Keyboard,MetaKeyboard)
+
+CSay("Mouse and Keyboard support present")
+
